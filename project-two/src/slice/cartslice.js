95585ff8dd3371af10f7efcbd4612f137d0d5767
nc636662-cyber <nc636662@gmail.com>
2026-04-14 11:28:28 +0530

    change

--- project-two/src/slice/cartslice.js
+++ project-two/src/slice/cartslice.js
@@ -16,23 +16,22 @@ const Wishlist = createSlice({
   initialState,
   reducers: {
 
-    // ✅ Add to wishlist
     addtowish: (state, action) => {
   const existItem = state.item.find(
     (item) => item.id === action.payload.id
   );
 
   if (existItem) {
-    // 👉 agar item already hai → quantity badhao
+   
     existItem.quantity += 1;
   } else {
-    // 👉 new item add karo with quantity = 1
+    
     state.item.push({ ...action.payload, quantity: 1 });
   }
   localStorage.setItem("wishlist", JSON.stringify(state.item));
 },
 
-    // ❌ Remove single item
+  
     removetowish: (state, action) => {
       state.item = state.item.filter(
         (item) => item.id !== action.payload
@@ -40,7 +39,7 @@ const Wishlist = createSlice({
       localStorage.setItem("wishlist", JSON.stringify(state.item));
     },
 
-    // 🧹 Remove all items
+  
     removeall: (state) => {
       state.item = [];
       localStorage.setItem("wishlist", JSON.stringify(state.item));
@@ -49,8 +48,8 @@ const Wishlist = createSlice({
   }
 });
 
-// ✅ Export actions
+
 export const { addtowish, removetowish, removeall } = Wishlist.actions;
 
-// ✅ Export reducer
+
 export default Wishlist.reducer;
